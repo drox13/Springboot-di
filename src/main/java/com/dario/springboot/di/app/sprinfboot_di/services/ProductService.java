f27dc938020f700e16ff50e1bee502d4e583d88c
drox13 <dario.baron@uptc.edu.co>
2024-10-24 10:55:13 -0500

    47 Service Class

---
+++ src/main/java/com/dario/springboot/di/app/sprinfboot_di/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.dario.springboot.di.app.sprinfboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.dario.springboot.di.app.sprinfboot_di.models.Product;
+import com.dario.springboot.di.app.sprinfboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+    private ProductRepository productRepository = new ProductRepository();
+
+    public List<Product> findAll(){
+        return productRepository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice()*1.25d;
+            p.setPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+    public Product findById(Long id){
+        return productRepository.findById(id);
+    }
+}
