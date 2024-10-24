1ed59871501aa56b4ecb38fa2875f706d94f1a3f
drox13 <dario.baron@uptc.edu.co>
2024-10-24 11:19:43 -0500

    48.  Controller class

---
+++ src/main/java/com/dario/springboot/di/app/sprinfboot_di/controllers/SomeController.java
@@ -0,0 +1,31 @@
+package com.dario.springboot.di.app.sprinfboot_di.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dario.springboot.di.app.sprinfboot_di.models.Product;
+import com.dario.springboot.di.app.sprinfboot_di.services.ProductService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> listProducts(){
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+    
+}
